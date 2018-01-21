c6c6b51dcc7071c8554e52d9ba78d16df9d143c4
ShaunZheng <zszshuang@gmail.com>
2018-01-21 23:42:57 +0800

    3-2

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -1,10 +1,11 @@
+# -*-coding:utf-8-*-
 from math import log
 
 def calcShannonEnt(dataSet):
 	numEntries = len(dataSet)
 	labelsCounts = {}
 	for featVec in dataSet:
-		print featVec
+		# print featVec
 		currentLabel = featVec[-1]
 		print currentLabel
 		if currentLabel not in labelsCounts.keys():
@@ -26,7 +27,29 @@ def createDataSet():
 	labels = ['no surfacing','flippers']
 	return dataSet,labels
 
+# 这个函数的功能是：讲第axis列的值和value比较，相同，则拿到除了axis列的数组，追加到retDataSet数组中
+def splitDataSet(dataSet,axis,value):
+	retDataSet = []
+	for featVec in dataSet:
+		# print featVec
+		# print featVec[:2]
+		# 从这可以看出axis指的是域（列）
+		if featVec[axis] == value:
+			# 这里是截取字符串，axis=0表示，0到0，为空。axis=1，表示0到1，截取数组里的第一个字符。
+			# 截取axis前面所有
+			reduceFeatVec = featVec[:axis]
+			# print reduceFeatVec
+			# 这行的主要目的是除去axis域那一列的值，只计算其他两列
+			# 截取axis后面所有，并和上面截取的合并
+			reduceFeatVec.extend(featVec[axis+1:])
+			# print reduceFeatVec
+			# 追加到数组retDataSet中
+			retDataSet.append(reduceFeatVec)
+	return retDataSet
+
 myDat,labels = createDataSet()
 
-print myDat
-print calcShannonEnt(myDat)
+# print myDat
+# print calcShannonEnt(myDat)
+
+print splitDataSet(myDat,1,1)
