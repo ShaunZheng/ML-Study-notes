30c214e3a16edb0d088932c0a7c86a1201c5ecd3
ShaunZheng <zszshuang@gmail.com>
2018-01-29 21:59:36 +0800

    4-5

--- Ch04/bayes.py
+++ Ch04/bayes.py
@@ -58,7 +58,7 @@ def testingNB():
 	p0V,p1V,pAb = trainNB0(trainMat,listClasses)
 	testEntry = ['love', 'my', 'dalmation']
 	thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
-	print thisDoc
+	# print thisDoc
 	print testEntry,'classified as :', classifyNB(thisDoc,p0V,p1V,pAb)
 	testEntry = ['stupid', 'garbage']
 	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
@@ -70,5 +70,45 @@ def bagOfWords2VecMN(vocabList, inputSet):
 		if word in vocabList:
 			returnVec[vocabList.index(word)] += 1
 	return returnVec
+
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+def spamTest():
+	docList = []; classList = []; fullText = []
+	for i in range(1,26):
+		wordList = textParse(open('email/spam/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+
+		wordList = textParse(open('email/ham/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+
+	vocabList = createVocabList(docList)
+	# 创造随机训练集testSet
+	trainingSet = range(50); testSet = []
+	for i in range(10):
+		randIndex = int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
 	
-testingNB()
+	trainMat = []; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V,p1V,pSpam = trainNB0(trainMat,trainClasses)
+	errorCount = 0
+
+	for docIndex in testSet:
+		wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+		if classifyNB(wordVector,p0V,p1V,pSpam) != classList[docIndex]:
+			errorCount += 1
+	
+	print '错误率为：',float(errorCount)/len(testSet)
+spamTest()
+
+# testingNB()
