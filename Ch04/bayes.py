0facbc4da86ce8832e969b2ef6d5bde662e71385
ShaunZheng <zszshuang@gmail.com>
2018-01-29 13:56:52 +0800

    4-4

--- Ch04/bayes.py
+++ Ch04/bayes.py
@@ -28,10 +28,10 @@ def trainNB0(trainMatrix,trainCategory):
 	numTrainDocs = len(trainMatrix)
 	numWords = len(trainMatrix[0])
 	pAbusive = sum(trainCategory)/float(numTrainDocs)
-	p0Num = zeros(numWords)
-	p1Num = zeros(numWords)
-	p0Denom = 0.0
-	p1Denom = 0.0
+	p0Num = ones(numWords)
+	p1Num = ones(numWords)
+	p0Denom = 2.0
+	p1Denom = 2.0
 	for i in range(numTrainDocs):
 		if trainCategory[i] == 1:
 			p1Num += trainMatrix[i]
@@ -39,21 +39,36 @@ def trainNB0(trainMatrix,trainCategory):
 		else:
 			p0Num += trainMatrix[i]
 			p0Denom += sum(trainMatrix[i])
-	p1Vect = p1Num/p1Denom
-	p0Vect = p0Num/p0Denom
+	p1Vect = log(p1Num/p1Denom)
+	p0Vect = log(p0Num/p0Denom)
 	return p0Vect,p1Vect,pAbusive
+def classifyNB(vec2ClassIfy,p0Vec,p1Vec,pClass1):
+	p1 = sum(vec2ClassIfy * p1Vec) + log(pClass1)
+	p0 = sum(vec2ClassIfy * p0Vec) + log(1.0 - pClass1)
+	if p1 > p0:
+		return 1
+	else:
+		return 0
+def testingNB():
+	listOPosts,listClasses = lodaDataSet()
+	myVocabList = createVocabList(listOPosts)
+	trainMat = []
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+	p0V,p1V,pAb = trainNB0(trainMat,listClasses)
+	testEntry = ['love', 'my', 'dalmation']
+	thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
+	print thisDoc
+	print testEntry,'classified as :', classifyNB(thisDoc,p0V,p1V,pAb)
+	testEntry = ['stupid', 'garbage']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry,'classified as :', classifyNB(thisDoc, p0V, p1V, pAb)
 
+def bagOfWords2VecMN(vocabList, inputSet):
+	returnVec = [0] * len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] += 1
+	return returnVec
 	
-listOPosts,listClasses = lodaDataSet()
-myVocabList = createVocabList(listOPosts)
-
-
-trainMat = []
-for postinDoc in listOPosts:
-	trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
-
-p0V,p1V,pAb = trainNB0(trainMat,listClasses)
-
-print pAb
-print p0V
-print p1V
+testingNB()
